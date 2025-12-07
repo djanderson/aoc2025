1e4b8b22fda96ef6a3dacdc47cb8f2bdf2cd3ac3
Douglas Anderson <douglas.j.anderson@pm.me>
2025-12-07 09:58:43 -0700

    Add day template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aoc2025
- Advent of Code 2025
+
+Advent of Code 2025 in Rust.
+
+Use `cd src/bin/dayN; cargo run --bin dayN` to run a specific day's challenge.
---
+++ src/bin/day/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn main() {
+    println!("part 1: {}", part1());
+    println!("part 2: {}", part2());
+}
+
+fn part1() -> u32 {
+    let input = fs::read_to_string("example.txt").unwrap();
+    todo!()
+}
+
+fn part2() -> u32 {
+    let input = fs::read_to_string("example.txt").unwrap();
+    todo!()
+}
